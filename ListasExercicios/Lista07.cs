eeaf80218d07cd5dfbc29b58bacf33f93dc86827
Frinng <167120714+Frinng@users.noreply.github.com>
2025-10-05 16:17:53 -0300

    Versao Oficial 2.0

--- ListasExercicios/Lista07.cs
+++ ListasExercicios/Lista07.cs
@@ -5,6 +5,8 @@
  *Hora da Criação: 15:01
  */
 
+using System.Formats.Asn1;
+
 namespace Lista03Exer;
 
 public class Lista07 {
@@ -18,6 +20,58 @@ public class Lista07 {
         //Variavel A
         int N1A = 0;
         string N1Astring;
+        int A = 0;
+        
+        //Variavel B
+        int N1B = 0;
+        string N1Bstring;
+        int B = 0;
+        int ResultB = 1;
+        
+        //Varivavel da Letra C
+        int N1C = 0;
+        string N1Cstring;
+        int SomaC = 0;
+        int C = 1;
+        
+        //Varivavel da Letra E
+        int N1E = 0;
+        string N1Estring;
+        int SomaE = 0;
+        int E = 1;
+        
+        //Variavel F
+        int N1F = 0;
+        string N1Fstring;
+        int F = 0;
+        
+        //Variavel G
+        int N1G = 0;
+        string N1Gstring;
+        int G = 0;
+        
+        //Variavel H
+        int N1H = 0;
+        string N1Hstring;
+        int H = 0;
+        int ResultH = 1;
+        
+        //Varivavel da Letra i
+        int N1I = 0;
+        string N1Istring;
+        int SomaI = 0;
+        int I = 1;
+        
+        //Varivavel da Letra E
+        int N1K = 0;
+        string N1Kstring;
+        int SomaK = 0;
+        int K = 1;
+        
+        //Variavel L
+        int N1L = 0;
+        string N1Lstring;
+        int L = 0;
 
         do {
             
@@ -53,24 +107,291 @@ public class Lista07 {
 
                     case 1:
                         Console.Clear();
-
-                        int i = 0;
                         
                         Console.WriteLine("Digite um numero:");
                         N1Astring = Console.ReadLine();
                         
-                        if (int.TryParse(N1Astring, out N1A)) {
-
+                        if (int.TryParse(N1Astring, out N1A) && N1A >= 0) {
+                            Console.Clear();
+                            
                             do {
 
-                                if (N1A % 2 == 0) {
+                                if (A % 2 == 0) {
                                     
-                                    Console.WriteLine(i);
+                                    Console.WriteLine(A);
                                     
                                 }
 
-                                i++;
-                            } while (i <= N1A);
+                                A++;
+                            } while (A <= N1A);
+                            Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                            
+                        }else{
+                        
+                            Funcoesuteis.DIGITEUMNUMEROINTEIRO();
+                        
+                        }
+                        
+                        break;
+                    case 2:
+                        Console.Clear();
+                        
+                        Console.WriteLine("Digite um numero:");
+                        N1Bstring = Console.ReadLine();
+
+                        if (int.TryParse(N1Bstring, out N1B)) {
+                            Console.Clear();
+                            
+                            do
+                            {
+
+                                ResultB = N1B * B;
+                                
+                                Console.WriteLine($"{N1B} X {B} = {ResultB}");
+
+                                B++;
+                            } while (B <= 10);
+                            
+                            Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                        }
+                        else {
+                            
+                            Funcoesuteis.DIGITEUMNUMEROINTEIRO();
+                            
+                        }
+                        
+                        break;
+                    case 3:
+                        Console.Clear();
+
+                        do {
+                            Console.WriteLine("Digite um numero:");
+                            N1Cstring = Console.ReadLine();
+
+                            if (int.TryParse(N1Cstring, out N1C)) {
+
+                                SomaC += N1C;
+
+                                C++;
+                            }else {
+                            
+                                Funcoesuteis.DIGITEUMNUMEROINTEIRO();
+                            
+                            }   
+                        } while (C <= 10);
+                        
+                        Console.Clear();
+                        
+                        Console.WriteLine($"O Resultado das somas dos numeros e {SomaC}");
+                        
+                        Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                        
+                        break;
+                    case 4:
+                        Console.Clear();
+                        
+                        Console.WriteLine("O Arquivo 1 ja conta para essa atividade.");
+                        
+                        Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                        
+                        break;
+                    case 5:
+                        Console.Clear();
+
+                        do
+                        {
+                            Console.WriteLine("Digite um numero:");
+                            N1Estring = Console.ReadLine();
+
+                            if (int.TryParse(N1Estring, out N1E)) {
+
+                                if (N1E > 0) {
+                                    SomaE += N1E;
+                                }
+
+                                E++;
+                            }else {
+                            
+                                Funcoesuteis.DIGITEUMNUMEROINTEIRO();
+                            
+                            }   
+                        } while (E <= 10);
+                        
+                        Console.Clear();
+                        
+                        Console.WriteLine($"O Resultado das somas dos numeros e {SomaE}");
+                        
+                        Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                        
+                        break;
+                    case 6:
+                        Console.Clear();
+                        
+                        Console.WriteLine("Digite um numero:");
+                        N1Fstring = Console.ReadLine();
+                        
+                        if (int.TryParse(N1Fstring, out N1F) && N1F >= 0) {
+                            Console.Clear();
+                            
+                            do {
+
+                                if (F % 2 == 0) {
+                                    
+                                    Console.WriteLine(F);
+                                    
+                                }
+
+                                F++;
+                            } while (F <= N1F);
+                            Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                            
+                        }else{
+                        
+                            Funcoesuteis.DIGITEUMNUMEROINTEIRO();
+                        
+                        }
+                        
+                        break;
+                    case 7:
+                        Console.Clear();
+                        
+                        Console.WriteLine("Digite um numero:");
+                        N1Gstring = Console.ReadLine();
+                        
+                        if (int.TryParse(N1Gstring, out N1G) && N1G >= 0) {
+                            Console.Clear();
+                            
+                            do {
+
+                                if (G % 2 == 0) {
+                                    
+                                    Console.WriteLine(G);
+                                    
+                                }
+
+                                F++;
+                            } while (G <= N1G);
+                            Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                            
+                        }else{
+                        
+                            Funcoesuteis.DIGITEUMNUMEROINTEIRO();
+                        
+                        }
+                        
+                        break;
+                    case 8:
+                        Console.Clear();
+                        
+                        Console.WriteLine("Digite um numero:");
+                        N1Hstring = Console.ReadLine();
+
+                        if (int.TryParse(N1Hstring, out N1H)) {
+                            Console.Clear();
+                            
+                            do
+                            {
+
+                                ResultH = N1H * H;
+                                
+                                Console.WriteLine($"{N1H} X {H} = {ResultH}");
+
+                                H++;
+                            } while (H <= 10);
+                            
+                            Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                        }
+                        else {
+                            
+                            Funcoesuteis.DIGITEUMNUMEROINTEIRO();
+                            
+                        }
+                        
+                        break;
+                    case 9:
+                        Console.Clear();
+
+                        do {
+                            Console.WriteLine("Digite um numero:");
+                            N1Istring = Console.ReadLine();
+
+                            if (int.TryParse(N1Istring, out N1I)) {
+
+                                SomaI += N1I;
+
+                                I++;
+                            }else {
+                            
+                                Funcoesuteis.DIGITEUMNUMEROINTEIRO();
+                            
+                            }   
+                        } while (I <= 10);
+                        
+                        Console.Clear();
+                        
+                        Console.WriteLine($"O Resultado das somas dos numeros e {SomaI}");
+                        
+                        Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                        
+                        break;
+                    case 10:
+                        Console.Clear();
+                        
+                        Console.WriteLine("O Arquivo 1 ja conta para essa atividade.");
+
+                        Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                        
+                        break;
+                    case 11:
+                        Console.Clear();
+
+                        do
+                        {
+                            Console.WriteLine("Digite um numero:");
+                            N1Kstring = Console.ReadLine();
+
+                            if (int.TryParse(N1Kstring, out N1K)) {
+
+                                if (N1K > 0) {
+                                    SomaK += N1K;
+                                }
+
+                                K++;
+                            }else {
+                            
+                                Funcoesuteis.DIGITEUMNUMEROINTEIRO();
+                            
+                            }   
+                        } while (K <= 10);
+                        
+                        Console.Clear();
+                        
+                        Console.WriteLine($"O Resultado das somas dos numeros e {SomaK}");
+                        
+                        Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
+                        
+                        
+                        break;
+                    case 12:
+                        Console.Clear();
+                        
+                        Console.WriteLine("Digite um numero:");
+                        N1Lstring = Console.ReadLine();
+                        
+                        if (int.TryParse(N1Lstring, out N1L) && N1L >= 0) {
+                            Console.Clear();
+                            
+                            do {
+
+                                if (L % 2 == 0) {
+                                    
+                                    Console.WriteLine(L);
+                                    
+                                }
+
+                                L++;
+                            } while (L <= N1L);
+                            Funcoesuteis.CLIQUEAQUIPARACONTINUAR();
                             
                         }else{
                         
@@ -79,28 +400,6 @@ public class Lista07 {
                         }
                         
                         
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                    case 6:
-                        break;
-                    case 7:
-                        break;
-                    case 8:
-                        break;
-                    case 9:
-                        break;
-                    case 10:
-                        break;
-                    case 11:
-                        break;
-                    case 12:
                         break;
                     case 13:
                         
@@ -123,13 +422,11 @@ public class Lista07 {
                         break;
                     
                 }
-                
             }else {
                 
                 Funcoesuteis.DIGITEUMAOPCAOVALIDA();
                 
             }
-            
         } while (Continuar);
     }
 }
